e1546323941720ea449893feadd0a8f5d768238d
bahador <noor.bahadory786@gmail.com>
2023-07-18 15:27:50 +0100

    added new code

--- script.js
+++ script.js
@@ -195,3 +195,18 @@ $(function(){
     $("nav").slideToggle();
     $(this).toggleClass("active");
 });
+function convertToNumber(value) {
+  // Convert a string value to a number if possible
+  let number_value = Number(value);
+  if (Number.isNaN(number_value)) {
+    return 0;
+  } else {
+    return number_value;
+  }
+}
+document.getElementById("comment-btn").addEventListener("click", (event) => {
+  let element_comments_list = document.getElementById("comments-list");
+  let new_div = document.createElement("div");
+  new_div.innerText = document.getElementById("comment-field").value;
+  element_comments_list.appendChild(new_div);
+});
